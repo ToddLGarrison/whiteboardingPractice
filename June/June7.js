3c0bcca6d68042c3ecbf18a3ccd1d0998c00fc37
Todd Garrison <toddlgarrison@gmail.com>
2023-06-07 11:22:08 -0400

    June7

---
+++ June/June7.js
@@ -0,0 +1,7 @@
+// Given an array of integers, create a function that returns true if all numbers found in the array are even, 
+// otherwise return false
+
+
+
+//Given an array of arrays, create a function that puts all elements in a single array (without using the JS Array method .flat())
+//Given a word, create a function that returns the same word without any vowels
