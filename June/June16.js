8db14a3aeb82f65a7ddd369794edbb21859661ce
Todd Garrison <toddlgarrison@gmail.com>
2023-06-16 13:14:32 -0400

    Problem two - recursion

--- June/June16.js
+++ June/June16.js
@@ -33,4 +33,29 @@ function twoSum(nums, target) {
     }
 
 
+// https://leetcode.com/problems/merge-two-sorted-lists/solutions/2705782/js-recursion-with-exlanation/?envType=featured-list&envId=top-interview-questions
+    // You are given the heads of two sorted linked lists list1 and list2.
+    // Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+    // Return the head of the merged linked list.
 
+
+const mergeTwoLists = (listOne, listTwo) => {
+    let mergedList = listOne.concat(listTwo)
+    let sortedMergedList = mergedList.sort()
+
+    return sortedMergedList
+}
+
+//SOLUTION - I did not know what a linked list was
+
+var mergeTwoLists = function (l1, l2) {
+    if (!l1) return l2;
+    else if (!l2) return l1;
+    else if (l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2
+    }
+};
