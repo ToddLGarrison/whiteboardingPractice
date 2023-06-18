b409cf81fedeadfbe58cd3a8eba677fb947591ac
Todd Garrison <toddlgarrison@gmail.com>
2023-06-18 17:52:10 -0400

    Problem 2

--- June/June18.js
+++ June/June18.js
@@ -3,3 +3,55 @@
 // If the length of the array is 0, it should return init.
 // Please solve it without using the built-in Array.reduce method.
 
+
+//SUDO Code
+const reducer = (numbers, func, initialValue) => {
+
+    if (numbers.length === 0 ) {
+        return initialValue
+    } 
+
+    let totalValue = initialValue
+
+    for (let i = 0; i < numbers.length; i++) {
+        let totalValue = totalValue += numbers[a]        
+    }
+    return totalValue
+}
+
+//SOLUTION
+
+const reduce = (nums, fn, init) => {
+    let value = init
+    for(let i = 0; i < nums.length; i++){
+        value = fn(value, nums[i])
+    }
+    return val
+}
+
+
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method.
+
+const map = (array, fn) => {
+    let mappedArray = []
+
+    for(let i =0; i< array.length; i++){
+        let mappedArray = fun(array[i] + i)
+    }
+
+    return mappedArray
+}
+
+//SOLUTION
+
+const map = (array, fn) => {
+    let mappedArray = []
+    for(let i = 0; i < array.length; i++) {
+        mappedArray.push(fn(array[i], i))
+    }
+    return mappedArray
+}
