3c0c64ae15191588660368fda9e0d00c0100efd4
Todd Garrison <toddlgarrison@gmail.com>
2023-06-19 20:32:52 -0400

    question 2

--- June/June18.js
+++ June/June18.js
@@ -69,3 +69,20 @@ const cancellable = function(fn, args, t) {
     let cancelFn = () => clearInterval(timer);
     return cancelFn;
 };
+
+
+// Write a generator function that returns a generator object which yields the fibonacci sequence.
+// The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+// The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
+//SOLUTION - https://leetcode.com/problems/generate-fibonacci-sequence/solutions/3439825/yield-solution-with-comments/
+const fibonacciSequenceGenerator = fn() => {
+    let a = 0
+    let b = 1
+
+    while (true) {
+        yield a
+        [a,b] = [b, a+b]
+    }
+
+}
