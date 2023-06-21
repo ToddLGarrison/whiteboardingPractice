92ccf72f2270bacdfa1f503a2b2b55d138360a47
Todd Garrison <toddlgarrison@gmail.com>
2023-06-21 11:42:17 -0400

    June21

---
+++ June/June21.js
@@ -0,0 +1,15 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+// A word is a maximal substring consisting of non-space characters only
+
+//sudo
+
+const phrase = "Hello World"
+
+const lastWordLength = (string) => {
+    let splitPhrase = string.split(" ")
+    let lastWord = splitPhrase[splitPhrase.length - 1]
+
+    let lastWordLetterCount = lastWord.length
+
+    return lastWordLetterCount
+}
