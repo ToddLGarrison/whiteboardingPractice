e9c27c7099ee43f05fcda11f5e9548f462d26c4b
Todd Garrison <toddlgarrison@gmail.com>
2023-08-07 13:37:32 -0400

    Solution

--- August/August8.js
+++ August/August8.js
@@ -6,3 +6,18 @@
 //return 0 (for languages that do have them).
 
 // Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number) {
+    if (number < 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    for (let i = 3; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+        }
+    }
+
+    return sum;
+}
