82839e58dcfd2a88e8f4ae8e49d9e9c6377c8def
Todd Garrison <toddlgarrison@gmail.com>
2023-08-11 12:35:48 -0400

    Sudo code

--- August/August11.js
+++ August/August11.js
@@ -1,3 +1,20 @@
 //Move the first letter of each word to the end of it, 
 //then add "ay" to the end of the word. Leave punctuation marks untouched.
 
+function pigIt(str){
+    //spilt on " "
+    //for loop 
+    //pop 
+    //shift to the end of the word
+    //add 'ay' to the end of the word
+    //add to empty const
+
+        let pigLatin = []
+
+        let splitWord = str.split(" ")
+        for(let i=0; i<str.length; i++){
+
+        }
+
+        return pigLatin
+    }
