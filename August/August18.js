d2214c83a1c371d2fbac3738e3d9a03b91c6e4e5
Todd Garrison <toddlgarrison@gmail.com>
2023-08-18 15:47:25 -0400

    Problem 1

---
+++ August/August18.js
@@ -0,0 +1,8 @@
+// You probably know the "like" system from Facebook and other pages. 
+// People can "like" blog posts, pictures or other items. 
+// We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. 
+// It must return the display text as shown in the examples:
+
+// For 4 or more names, the number in "and 2 others" simply increases.
