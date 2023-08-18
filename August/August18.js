fe9044c6167ba303788360311ae2f48d746b4ecc
Todd Garrison <toddlgarrison@gmail.com>
2023-08-18 15:48:17 -0400

    solution

--- August/August18.js
+++ August/August18.js
@@ -6,3 +6,19 @@
 // It must return the display text as shown in the examples:
 
 // For 4 or more names, the number in "and 2 others" simply increases.
+
+//Solution
+
+function likes(names) {
+    if(names.length === 0){
+        return "no one likes this"
+    } else if (names.length === 1) {
+        return `${names[0]} likes this`
+    } else if (names.length === 2) {
+        return `${names[0]} and ${names[1]} like this`
+    } else if (names.length === 3){
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else if (names.length >= 4){
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    }
+}
