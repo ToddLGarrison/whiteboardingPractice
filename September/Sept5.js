504ee960c7874d4807603e64aca053ed3f953ba1
Todd Garrison <toddlgarrison@gmail.com>
2023-09-05 16:10:01 -0400

    Test 1

--- September/Sept5.js
+++ September/Sept5.js
@@ -20,3 +20,43 @@ const splitStringIntoPairs = (inputString) => {
     }
     return pairs
 }
+
+
+//QUESTION 2
+
+// Given an array (arr) as an argument complete the function countSmileys 
+//that should return the total number of smiling faces.
+
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+// Example
+
+// function countSmileys(arr) {
+//     return 0;
+//   }
+
+const countSmileys = (array) => {
+    if(array.length === 0){
+        return 0;  
+    }
+        
+
+    let pattern = /[:;][-~]?[)D]/
+
+    let count = 0;
+
+    for (const face of array) {
+        if (pattern.test(face)){
+            count ++
+        }
+    }
+    return count;
+}
