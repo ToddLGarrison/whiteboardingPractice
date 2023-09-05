56fca410f9b82f008babc758e481f19f04b7e330
Todd Garrison <toddlgarrison@gmail.com>
2023-09-05 15:51:13 -0400

    Solution

--- September/Sept5.js
+++ September/Sept5.js
@@ -7,3 +7,16 @@
 // * 'abc' =>  ['ab', 'c_']
 // * 'abcdef' => ['ab', 'cd', 'ef']
 
+const splitStringIntoPairs = (inputString) => {
+    if (inputString.length % 2 === 1) {
+        inputString += '_'
+    }
+
+    let pairs = []
+
+    for (let i = 0; i < inputString.length; i += 2) {
+        let pair = inputString.slice(i, i + 2)
+        pairs.push(pair)
+    }
+    return pairs
+}
