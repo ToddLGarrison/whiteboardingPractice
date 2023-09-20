4737449e3369068a5dc2747f38fe7fa42b1f2886
Todd Garrison <toddlgarrison@gmail.com>
2023-09-20 13:39:10 -0400

    Solution 2

--- September/Sept20.js
+++ September/Sept20.js
@@ -16,3 +16,8 @@ var createCounter = function(n) {
 // Given a positive integer millis, write an asynchronous function that sleeps for 
 // millis milliseconds. It can resolve any value.
 
+async function sleep(millis) {
+    return new Promise (resolve => {
+        setTimeout(resolve, millis)
+    })
+}
