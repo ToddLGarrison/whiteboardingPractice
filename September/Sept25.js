6c812898f214e39aa98d4eb42036e801529319e3
Todd Garrison <toddlgarrison@gmail.com>
2023-09-25 12:04:15 -0400

    Solution 2

--- September/Sept25.js
+++ September/Sept25.js
@@ -4,3 +4,22 @@ var fibGenerator = function*(a = 0, b=1) {
     yield a;
     yield* fibGenerator(b, a+b);
 };
+
+
+// Your goal in this kata is to implement a difference function, 
+// which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b keeping their order.
+
+function arrayDiff(a, b) {
+    let remainingNumbers = []
+
+    for(let i = 0; i < a.length; i++) {
+        let currentNumber = a[i];
+
+        if (!b.includes(currentNumber)){
+            remainingNumbers.push(a[i])
+        }
+    }
+
+    return remainingNumbers
+}
