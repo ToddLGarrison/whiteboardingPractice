6659e914cb0cb158ea40e2694e4a0c8e3dbde4cc
Todd Garrison <toddlgarrison@gmail.com>
2023-09-25 11:55:31 -0400

    Solution

--- September/Sept25.js
+++ September/Sept25.js
@@ -1,1 +1,6 @@
 //Write a generator function that returns a generator object which yields the fibonacci sequence.
+
+var fibGenerator = function*(a = 0, b=1) {
+    yield a;
+    yield* fibGenerator(b, a+b);
+};
