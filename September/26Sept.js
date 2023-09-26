cf8148469a6728330ea962dcb041e6422bdb21d7
Todd Garrison <toddlgarrison@gmail.com>
2023-09-26 13:57:04 -0400

    Solution

--- September/26Sept.js
+++ September/26Sept.js
@@ -6,10 +6,42 @@
 // // The passed in string will only consist of alphabetical characters and spaces(' '). 
 // Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
 
+//SUDO Attempt
 function toWeirdCase(string){
     let weirdCase = ""
 
-    
+    let splitString = string.split(" ")
+
+    for (let i = 0; i < splitString.length; i++){
+
+        if (splitString[i] % 2 === 0){
+            weirdCase.push(splitString[i].toUpperCase())
+        } else {
+            weirdCase.push(splitString[i])
+        }
+
+    }
 
     return weirdCase
+}
+
+//Solution
+function toWeirdCase(string){
+    let words = string.split(' ')
+
+    function toggleCase(word) {
+        let result = ''
+
+        for (let i = 0; i < word.length; i++){
+            if (i % 2 ===0){
+                result += word[i].toUpperCase()
+            } else {
+                result += word[i].toLowerCase()
+            }
+        }
+        return result
+    }
+    weirdString = words.map(toggleCase)
+
+    return weirdString.join(' ')
 }
