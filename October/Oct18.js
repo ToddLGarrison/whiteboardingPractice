99ba06314cc683aa54f1ea836dcae6ad02452fda
Todd Garrison <toddlgarrison@gmail.com>
2023-10-18 14:40:11 -0400

    Problem 2

--- October/Oct18.js
+++ October/Oct18.js
@@ -16,3 +16,34 @@ function twoSums(nums, target) {
 
     return []
 }
+
+
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+// Note: This challenge introduces precision problems. 
+
+function plusMinus(arr) {
+    let positiveCount = 0
+    let negativeCount = 0
+    let zeroCount = 0
+
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] > 0) {
+            positiveCount += 1
+        } else if (arr[i] < 0) {
+            negativeCount += 1
+        } else {
+            zeroCount += 1
+        }
+    }
+
+
+    const total = arr.length
+    const positiveRatio = positiveCount/total
+    const negativeRatio = negativeCount/total
+    const zeroRatio = zeroCount/total
+
+    console.log(positiveRatio.toFixed(6))
+    console.log(negativeRatio.toFixed(6))
+    console.log(zeroRatio.toFixed(6))
+}
