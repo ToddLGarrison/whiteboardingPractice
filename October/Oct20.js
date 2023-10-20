83521a9052b28f51bfbd25f02a683456ca60a6af
Todd Garrison <toddlgarrison@gmail.com>
2023-10-20 11:20:34 -0400

    problem 3

--- October/Oct20.js
+++ October/Oct20.js
@@ -26,3 +26,34 @@ function diagonalDifference(arr) {
     return (diagonalDifference)
 
 }
+
+// Given a list of integers, count and return the number of times each value appears as an array of integers.
+
+function countingSort(arr) {
+    const frequencyArray = new Array(100).fill(0);
+
+    // Iterate through the input array
+    for (let i = 0; i < arr.length; i++) {
+        const currentNumber = arr[i];
+
+        // For each number in the input array, iterate through the frequency array
+        for (let j = 0; j < frequencyArray.length; j++) {
+            if (j === currentNumber) {
+                frequencyArray[j]++;
+            }
+        }
+    }
+
+    return frequencyArray;
+}
+
+//OF
+
+function countingSort(arr) {
+    const frequencyArray = new Array(100).fill(0)
+
+    for (let i = 0; i < arr.length; i++) {
+        frequencyArray[arr[i]]++
+    }
+    return frequencyArray
+}
