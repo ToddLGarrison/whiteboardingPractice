51718c71a71b5236ff0aae1e26d79f732f250185
Todd Garrison <toddlgarrison@gmail.com>
2023-10-20 10:51:48 -0400

    problem 2

--- October/Oct20.js
+++ October/Oct20.js
@@ -8,3 +8,21 @@ function lonelyinteger(a) {
     }
     return soloNumber
 }
+
+//Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+function diagonalDifference(arr) {
+    let leftDiagonal = 0
+    let rightDiagonal = 0
+
+
+    for (let i = 0; i < arr.length; i++) {
+        leftDiagonal += arr[i][i]
+        rightDiagonal += arr[i][arr.length - 1 - i]
+    }
+
+    let diagonalDifference = Math.abs(leftDiagonal - rightDiagonal)
+
+    return (diagonalDifference)
+
+}
