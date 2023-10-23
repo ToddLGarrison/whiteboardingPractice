b20ac2ae9b93e3b41168c58f24a5649e8c5d892e
Todd Garrison <toddlgarrison@gmail.com>
2023-10-23 12:46:35 -0400

    Problem 2

--- October/Oct23.js
+++ October/Oct23.js
@@ -7,3 +7,30 @@ function towerBreakers(n, m) {
         return 1
     }
 }
+
+// Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet.
+
+function caesarCipher(s, k) {
+    let codedString = ''
+    let splitString = s.split('')
+    
+    for(let i = 0; i < splitString.length; i++){
+        if (splitString[i].match(/[a-zA-z]/)){
+            let isUpperCase = splitString[i] === splitString[i].toUpperCase()
+            let characterCode = splitString[i].charCodeAt(0)
+            let shift = isUpperCase ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
+            
+            let shiftedCharacterCode = 
+                (characterCode - shift + k) % 26 + shift
+            let shiftedCharacter = String.fromCharCode(shiftedCharacterCode)
+            
+            codedString += shiftedCharacter
+        } else if (splitString[i] === ' ') {
+            codedString += '-'
+        } else {
+            codedString += splitString[i]
+        }
+    }
+    
+    return codedString
+}
