867f34e3889119b67e361db62b57a656fc4329f9
Todd Garrison <toddlgarrison@gmail.com>
2023-11-17 16:37:55 -0500

    create hangman

---
+++ hangman/hangman.js
@@ -0,0 +1,11 @@
+const readLineSync = require('readline-sync')
+
+let word = 'arise'
+let splitWord = word.split('')
+
+let guessedWord = Array(splitWord.length).fill('_')
+let incorrectGuess = 0
+
+function displayWord = () {
+
+}
