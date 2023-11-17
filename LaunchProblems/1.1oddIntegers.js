a4764de4fe6ab9454ac4f2ffd83a254ee1ff37f1
Todd Garrison <toddlgarrison@gmail.com>
2023-11-17 14:25:43 -0500

    Solution 1

--- LaunchProblems/1.1oddIntegers.js
+++ LaunchProblems/1.1oddIntegers.js
@@ -1,1 +1,19 @@
 // Given an array of integers, return a list of all odd numbers found in the list
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+const oddNumbers = (numbers) => {
+    oddArray = []
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 != 0) {
+            oddArray.push(numbers[i])
+        }
+    }
+    return oddArray
+}
+
+const oddInts = (numbers) => {
+    return numbers.filter(number => number % 2 !== 0)
+    
+}
