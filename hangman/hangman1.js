0673d9a0ee5bf22d5663f5519967b7747ca9f0de
Todd Garrison <toddlgarrison@gmail.com>
2023-11-17 17:31:28 -0500

    update user experience

--- hangman/hangman1.js
+++ hangman/hangman1.js
@@ -5,9 +5,33 @@ let splitWord = word.split('')
 
 let incorrectGuessCount = 0
 let guessedWord = Array(splitWord.length).fill('_')
+let incorrectLetters = []
 
 function displayWord() {
     console.log('Word: ', guessedWord)
     console.log('Incorrect guess count: ', incorrectGuessCount)
     console.log('Remaining guesses: ', (6 - incorrectGuessCount))
 }
+
+function checkGuess() {
+    let guess = readLineSync.keyIn('Please enter your guess as a single lowercase letter', { limit: 'abcdefghijklmnopqrstuvwxyz'}).toLocaleLowerCaseString()
+
+        if (splitWord.includes(guess)){
+            for (let i = 0; i < splitWord.length; i++) {
+                if(splitWord[i] === guess) {
+                    guessedWord[i] = guess
+                }
+            }
+        } else {
+            console.log('Wrong. Word does not contain: ', guess)
+            incorrectGuessCount++
+        }
+
+        
+
+        checkGuess()
+    }
+
+
+displayWord()
+checkGuess()
