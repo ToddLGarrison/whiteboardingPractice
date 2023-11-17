b9345f55b5fdd7708145bd81542707151630cab3
Todd Garrison <toddlgarrison@gmail.com>
2023-11-17 14:39:09 -0500

    Problem 2

---
+++ LaunchProblems/1.2double.js
@@ -0,0 +1,7 @@
+// Given an array of integers, return a new list that doubles each value
+
+let array = [1, 2, 3, 4, 5, 6, 7];
+
+const doubleArray = (array) => {
+    return array.map((num) => num * 2)
+}
