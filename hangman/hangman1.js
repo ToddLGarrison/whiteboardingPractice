22eea0830a8a159ed675328d58c9565548bb2afc
Todd Garrison <toddlgarrison@gmail.com>
2023-11-17 17:09:22 -0500

    new attempt hangman, add user feature

--- hangman/hangman1.js
+++ hangman/hangman1.js
@@ -5,3 +5,9 @@ let splitWord = word.split('')
 
 let incorrectGuessCount = 0
 let guessedWord = Array(splitWord.length).fill('_')
+
+function displayWord() {
+    console.log('Word: ', guessedWord)
+    console.log('Incorrect guess count: ', incorrectGuessCount)
+    console.log('Remaining guesses: ', (6 - incorrectGuessCount))
+}
