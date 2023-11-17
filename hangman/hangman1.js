ea96de5cc6e2b1a94811341f7c24c13e83878ea3
Todd Garrison <toddlgarrison@gmail.com>
2023-11-17 17:39:37 -0500

    fix error

--- hangman/hangman1.js
+++ hangman/hangman1.js
@@ -3,18 +3,18 @@ const readLineSync = require('readline-sync')
 let word = 'monstera'
 let splitWord = word.split('')
 
-let incorrectGuessCount = 0
 let guessedWord = Array(splitWord.length).fill('_')
+let incorrectGuessCount = 0
 let incorrectLetters = []
 
 function displayWord() {
-    console.log('Word: ', guessedWord)
+    console.log('Word: ', guessedWord.join(' '))
     console.log('Incorrect guess count: ', incorrectGuessCount)
     console.log('Remaining guesses: ', (6 - incorrectGuessCount))
 }
 
 function checkGuess() {
-    let guess = readLineSync.keyIn('Please enter your guess as a single lowercase letter', { limit: 'abcdefghijklmnopqrstuvwxyz'}).toLocaleLowerCaseString()
+    let guess = readLineSync.keyIn('Please enter your guess as a single lowercase letter: ', { limit: 'abcdefghijklmnopqrstuvwxyz'}).toLowerCase()
 
         if (splitWord.includes(guess)){
             for (let i = 0; i < splitWord.length; i++) {
@@ -23,11 +23,22 @@ function checkGuess() {
                 }
             }
         } else {
+            incorrectLetters.push(guess)
             console.log('Wrong. Word does not contain: ', guess)
             incorrectGuessCount++
         }
 
-        
+        if (guessedWord.join('') === word) {
+            console.log('Congratulations, you guessed the word')
+            return
+        } else {
+            displayWord()
+        }
+
+        if (incorrectGuessCount > 6){
+            console.log('You lose. Too many incorrect guesses')
+            return
+        }
 
         checkGuess()
     }
