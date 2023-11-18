20d4295c3df908621d5fae3dc342cbd39f655309
Todd Garrison <toddlgarrison@gmail.com>
2023-11-18 14:32:45 -0500

    evenArray function

---
+++ LaunchProblems/2.3evenNums.js
@@ -0,0 +1,14 @@
+// Given an array of integers, create a function that returns true 
+// if all numbers found in the array are even, otherwise return false
+
+const arrayEven = [2, 4, 6, 8]
+const arrayBoth = [1, 3, 6, 8]
+
+const areEven = (array) = () => {
+    array.every(function(number){
+        return number % 2 === 0
+    } )
+}
+
+areEven(arrayEven)
+areEven(arrayBoth)
