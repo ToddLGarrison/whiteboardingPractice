a26ebca34f9018781a1d38741fd06c62f8ff37bb
Todd Garrison <toddlgarrison@gmail.com>
2023-11-19 11:02:43 -0500

    arrayOfArrays

---
+++ LaunchProblems/3.1arrayOfArrays.js
@@ -0,0 +1,13 @@
+// Given an array of arrays, create a function that puts all elements in a single array 
+// (without using the JS Array method .flat())
+
+const arrayOfArrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+const createOneArray = (arrayOfArrays) => {
+    let theOneTrueArray = []
+
+    
+    return theOneTrueArray
+}
+
+createOneArray()
