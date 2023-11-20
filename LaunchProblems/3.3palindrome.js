d35472ba17a56a71f4111e012e366b88cd69c784
Todd Garrison <toddlgarrison@gmail.com>
2023-11-20 17:27:11 -0500

    solution

--- LaunchProblems/3.3palindrome.js
+++ LaunchProblems/3.3palindrome.js
@@ -1,15 +1,17 @@
 //given a word check if it is a palindrome return true or false
 
 const findPalindrome = (word) => {
-    for (let i = 0; j = word.length - 1; i < j; i++, j--) {
-        if(word[i] === word[j]) {
-            return true;
-        } else {
+    for (let i = 0, j = word.length - 1; i < j; i++, j--) {
+        if (word[i] !== word[j]) {
             return false;
         }
     }
+    return true;
 }
 
 
-console.log('test 1:', findPalindrome(toot) === true)
-console.log('test 2:', findPalindrome(fact) === false)
+console.log('test 1:', findPalindrome('toot') === true)
+console.log('test 2:', findPalindrome('fact') === false)
+console.log('test 3:', findPalindrome('racecar') === true)
+console.log('test 4:', findPalindrome('tort') === false)
+
