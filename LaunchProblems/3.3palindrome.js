c52ee9e2dc3243eb2f6fe23c0325d1094f64ca67
Todd Garrison <toddlgarrison@gmail.com>
2023-11-20 17:18:54 -0500

    Attempt 1

--- LaunchProblems/3.3palindrome.js
+++ LaunchProblems/3.3palindrome.js
@@ -1,8 +1,13 @@
 //given a word check if it is a palindrome return true or false
 
 const findPalindrome = (word) => {
-
-
+    for (let i = 0; j = word.length - 1; i < j; i++, j--) {
+        if(word[i] === word[j]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 
