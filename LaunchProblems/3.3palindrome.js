939d9906b96c810940c9c556c338f59e06a495af
Todd Garrison <toddlgarrison@gmail.com>
2023-11-20 17:01:39 -0500

    palindrome

---
+++ LaunchProblems/3.3palindrome.js
@@ -0,0 +1,10 @@
+//given a word check if it is a palindrome return true or false
+
+const findPalindrome = (word) => {
+
+
+}
+
+
+console.log('test 1:', findPalindrome(toot) === true)
+console.log('test 2:', findPalindrome(fact) === false)
