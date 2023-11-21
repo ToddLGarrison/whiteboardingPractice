0b8d5391974bd7a1832e4eb52c801d2afca231a8
Todd Garrison <toddlgarrison@gmail.com>
2023-11-21 16:33:31 -0500

    multiplication

---
+++ LaunchProblems/4.2multiply.js
@@ -0,0 +1,18 @@
+// Write a multiplication function without using the * operation. 
+// Determine what arguments you think this function will accept. 
+// The return of the function should be the product of the function
+
+const number1 = 9
+const number2 = 8
+
+const multiplication = (number1, number2) => {
+    let total = 0
+
+    for (let i = 0; i < number2; i++) {
+        total = total + number1
+    }
+
+    return total
+}
+
+console.log('test1', multiplication(number1, number2))
