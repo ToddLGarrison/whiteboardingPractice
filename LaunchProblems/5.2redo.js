89f4956a12e2bf6a56f36eb73c7fe7a35b4446eb
Todd Garrison <toddlgarrison@gmail.com>
2023-11-22 14:46:22 -0500

    redone again

--- LaunchProblems/5.2redo.js
+++ LaunchProblems/5.2redo.js
@@ -7,6 +7,15 @@ const string = 'carrot'
 const letterLocation = (string) => {
     let letterHash = {}
 
+    for (let i = 0; i < string.length; i++) {
+        let letter = string[i]
+
+        if (letterHash[letter]) {
+            letterHash[letter].push(i)
+        } else {
+            letterHash[letter] = [i]
+        }
+    }
 
     return letterHash
 }
