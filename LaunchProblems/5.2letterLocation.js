bef6ca5691e94bba1cf54699962e6060c0a5c8c1
Todd Garrison <toddlgarrison@gmail.com>
2023-11-22 13:56:59 -0500

    problem 2

---
+++ LaunchProblems/5.2letterLocation.js
@@ -0,0 +1,5 @@
+// Given a string, write a function to return a hash/object that, for each letter, 
+// stores an array of indices where that letter can be found. 
+// For example, in the word "bee", "b" can be found at index 0, and "e" can be found at index 1 & 2
+
+const string = 'carrot'
