14121974de5c38eb7bcc287473971a8d2cb6ab58
Todd Garrison <toddlgarrison@gmail.com>
2023-11-23 12:42:35 -0500

    solution

--- LaunchProblems/6.1frequentInteger.js
+++ LaunchProblems/6.1frequentInteger.js
@@ -3,7 +3,26 @@
 const integerArray = [1, 1, 1, 2, 2, 3, 4, 5, 5]
 
 const mostFrequentInteger = (array) => {
+    let frequencyHash = {}
+    let maxFrequency = 0
+    let mostFrequentInteger = null
 
+    for (let i = 0; i < array.length; i++) {
+        let number = array[i]
+
+        if (frequencyHash[number]){
+            frequencyHash[number]++ 
+        } else {
+            frequencyHash[number] = 1
+        }
+    
+        if(frequencyHash[number] > maxFrequency){
+            mostFrequentInteger = number
+            maxFrequency = frequencyHash[number]
+        }
+    }
+
+    return mostFrequentInteger
 }
 
-console.log('test 1:', mostFrequentInteger);
+console.log('test 1:', mostFrequentInteger(integerArray));
