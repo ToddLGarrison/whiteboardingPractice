0fb2862a331631b040e5733bb74a9979a6b96a4e
Todd Garrison <toddlgarrison@gmail.com>
2023-11-23 12:21:58 -0500

    problem1

---
+++ LaunchProblems/6.1frequentInteger.js
@@ -0,0 +1,9 @@
+// Given an array of integers, write a function to find the most frequent integer in the array
+
+const integerArray = [1, 1, 1, 2, 2, 3, 4, 5, 5]
+
+const mostFrequentInteger = (array) => {
+
+}
+
+console.log('test 1:', mostFrequentInteger);
