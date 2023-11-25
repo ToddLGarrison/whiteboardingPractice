8f41dc28765304dbe510b500ddafd22dfd10acfc
Todd Garrison <toddlgarrison@gmail.com>
2023-11-25 10:33:47 -0500

    solution

--- LaunchProblems/7.1redo.js
+++ LaunchProblems/7.1redo.js
@@ -1,7 +1,12 @@
 //create a factorial function
 
-const factorial = () => {
+const factorial = (integer) => {
+    let result = 1
 
+    for (let i = 1; i <= integer; i++) {
+        result *= i
+    }
+    return result
 }
 
 console.log('test: ', factorial(8))
