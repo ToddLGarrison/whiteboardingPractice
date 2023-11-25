44ad7a72e24dd0a02c709a78995d7a6eba8dc87c
Todd Garrison <toddlgarrison@gmail.com>
2023-11-25 10:43:06 -0500

    fibonacci

---
+++ LaunchProblems/7.2fibonacci.js
@@ -0,0 +1,13 @@
+//write a function that will output the fibonacci sequence to an input 
+
+const fibonacci = (integer) => {
+    let sequence = [0, 1]
+
+    for (let i = 0; i <= integer; i++) {
+        sequence.push((sequence[0] + sequence[1]))
+    }
+
+    return sequence
+}
+
+console.log('test: ', fibonacci(6))
