b55fa89c8ca0fec1e49892674f5b4f4ded820f7d
Todd Garrison <toddlgarrison@gmail.com>
2023-11-26 13:00:44 -0500

    set up output display

--- hangman/hangman2.js
+++ hangman/hangman2.js
@@ -1,12 +1,29 @@
-let readLineSync = require('readLineSync');
+const readLineSync = require('readLine-sync');
 
-let word = 'Mighty'
+let word = 'mighty'
 let splitWord = word.split('')
 let incorrectGuessCount = 0
-
-
+let maxIncorrectGuess = 6
 
 //create blank guessedWord 
 
 let guessedWord = Array(splitWord.length).fill('_')
 
+//create display for after each guess
+
+function displayResults () {
+    console.log(guessedWord)
+    console.log("Number of incorrect guesses", incorrectGuessCount)
+    console.log('Number of incorrect guesses remaining', maxIncorrectGuess - incorrectGuessCount)
+}
+
+//create the game
+
+function hangman() {
+    let guess = readLineSync.
+
+}
+
+hangman()
+
+displayResults()
