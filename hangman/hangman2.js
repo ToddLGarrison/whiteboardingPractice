58dacb3123a202d10730662e8617837dc28361e8
Todd Garrison <toddlgarrison@gmail.com>
2023-11-26 12:54:49 -0500

    hangman attempt

---
+++ hangman/hangman2.js
@@ -0,0 +1,10 @@
+let readLineSync = require('readLineSync');
+
+let word = 'Mighty'
+let splitWord = word.split('')
+
+
+//create blank guessedWord 
+
+let guessedWord = Array(splitWord.length, '_')
+
