247c4114f5d844f8e4e7fe6be6498f8d202bb445
Todd Garrison <toddlgarrison@gmail.com>
2023-11-26 12:56:09 -0500

    set up

--- hangman/hangman2.js
+++ hangman/hangman2.js
@@ -2,9 +2,11 @@ let readLineSync = require('readLineSync');
 
 let word = 'Mighty'
 let splitWord = word.split('')
+let incorrectGuessCount = 0
+
 
 
 //create blank guessedWord 
 
-let guessedWord = Array(splitWord.length, '_')
+let guessedWord = Array(splitWord.length).fill('_')
 
