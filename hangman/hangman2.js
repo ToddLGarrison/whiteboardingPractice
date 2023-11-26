2683ecc2ebf66c5ed0b53ed0f304192063d8dfe7
Todd Garrison <toddlgarrison@gmail.com>
2023-11-26 13:17:31 -0500

    solution

--- hangman/hangman2.js
+++ hangman/hangman2.js
@@ -4,6 +4,7 @@ let word = 'mighty'
 let splitWord = word.split('')
 let incorrectGuessCount = 0
 let maxIncorrectGuess = 6
+let incorrectLetters = []
 
 //create blank guessedWord 
 
@@ -12,18 +13,50 @@ let guessedWord = Array(splitWord.length).fill('_')
 //create display for after each guess
 
 function displayResults () {
-    console.log(guessedWord)
-    console.log("Number of incorrect guesses", incorrectGuessCount)
-    console.log('Number of incorrect guesses remaining', maxIncorrectGuess - incorrectGuessCount)
+    console.log('Word: ', guessedWord.join(' '))
+    console.log("Number of incorrect guesses ", incorrectGuessCount)
+    console.log('Number of incorrect guesses remaining', (maxIncorrectGuess - incorrectGuessCount))
+    console.log('Incorrect letters: ', incorrectLetters)
 }
 
 //create the game
 
 function hangman() {
-    let guess = readLineSync.
+    //set up guess for node
+    let guess = readLineSync.keyIn('Please guess a letter, enter as a lowercase letter: ', { limit: 'abcdefghijklmnopqrstuvwxyz'}).toLowerCase()
 
+    //check guess against letters
+    if (splitWord.includes(guess)) {
+        //loop to push guess letter into correct index
+        for (let i = 0; i < splitWord.length; i++) {
+            if(splitWord[i] === guess) {
+                guessedWord[i] = guess
+            }
+        } 
+        //set handle incorrect guess
+    } else {
+        console.log('Incorrect, word does not contain: ', guess)
+        incorrectLetters.push(guess)
+        incorrectGuessCount++
+    }
+
+    //handle winning
+    if(guessedWord.join('') === word) {
+        console.log('Congratulations you have guessed the word: ', word)
+        return
+    } else {
+        displayResults()
+    }
+
+    //handle losing
+    if(incorrectGuessCount >= maxIncorrectGuess) {
+        console.log('You lose')
+        return
+    }
+
+    //call for continuous play
+    hangman()
 }
 
-hangman()
-
 displayResults()
+hangman()
