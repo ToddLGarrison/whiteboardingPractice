063d96fb4e48e9c075855d8f4e6179d1429b030f
Todd Garrison <toddlgarrison@gmail.com>
2023-12-01 16:32:54 -0500

    Launch problems round 2

---
+++ LaunchProblems2/1.1oddIntegers.js
@@ -0,0 +1,17 @@
+// Given an array of integers, return a list of all odd numbers found in the list
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+const oddNumbers = (array) => {
+    let oddNumbers = []
+
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] % 2 !== 0) {
+            oddNumbers.push(array[i])
+        }
+    }
+
+    return oddNumbers
+}
+
+console.log("Test 1", oddNumbers(numbers))
