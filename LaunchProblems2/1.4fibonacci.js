53c04a6fddc6d6a17e0e053c35cd9afc0843b378
Todd Garrison <toddlgarrison@gmail.com>
2023-12-04 13:41:49 -0500

    advent of code

---
+++ LaunchProblems2/1.4fibonacci.js
@@ -0,0 +1,14 @@
+//write a function that will output the fibonacci sequence to an input 
+
+const fibonacciFunction = (integer) => {
+    let sequence = [0, 1]
+
+    for (let i = 2; i < integer; i++) {
+        let nextNumber = sequence[sequence.length - 2] + sequence[sequence.length - 1]
+        sequence.push(nextNumber)
+    }
+
+    return sequence
+}
+
+console.log('test', fibonacciFunction(10))
