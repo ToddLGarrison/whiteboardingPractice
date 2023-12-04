dde8ea17357c531fd3ced75cd58e546c02feb586
Todd Garrison <toddlgarrison@gmail.com>
2023-12-04 13:49:57 -0500

    set up

---
+++ adventOfCode/day1.js
@@ -0,0 +1,15 @@
+const adventDayOne = (array) => {
+    let doubleDigits = 0
+
+    for (let i = 0; i < array.length; i++) {
+        let firstNumber = null
+        let secondNumber = null
+
+
+    }
+
+    
+    return 
+}
+
+console.log(adventDayOne(dataSet))
