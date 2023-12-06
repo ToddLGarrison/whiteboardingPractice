9285812c3107c40634f5f2a66d940128d71dfd42
Todd Garrison <toddlgarrison@gmail.com>
2023-12-06 13:18:23 -0500

    daily challenge 2

---
+++ LaunchProblems2/2.2upperLowerCase.js
@@ -0,0 +1,4 @@
+// Given a string, return a new string where each character that was 
+// lowercase is now uppercase, and each character that was uppercase is now lowercase
+
+string = "TOMOrrow yOU WIll EAT LUNCH"
