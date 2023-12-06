f4114fb146d45e4a96bc37e587884001c9b47689
Todd Garrison <toddlgarrison@gmail.com>
2023-12-06 13:24:53 -0500

    solution

--- LaunchProblems2/2.2upperLowerCase.js
+++ LaunchProblems2/2.2upperLowerCase.js
@@ -2,3 +2,22 @@
 // lowercase is now uppercase, and each character that was uppercase is now lowercase
 
 string = "TOMOrrow yOU WIll EAT LUNCH"
+
+const changeCase = (string) => {
+    let changedCharacters = ""
+    let splitCharacters = string.split('')
+
+    for (let i = 0; i < splitCharacters.length; i++) {
+        if (splitCharacters[i] === splitCharacters[i].toUpperCase()) {
+            splitCharacters[i] = splitCharacters[i].toLowerCase()
+        } else {
+            splitCharacters[i] = splitCharacters[i].toUpperCase()
+        }
+    }
+
+    changedCharacters = splitCharacters.join('')
+
+    return changedCharacters
+}
+
+console.log(changeCase(string))
