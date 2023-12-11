b5c3f997597c769ecdd3ddb628236e802bf77d53
Todd Garrison <toddlgarrison@gmail.com>
2023-12-11 14:22:36 -0500

    attempt 4 set up

---
+++ hangman/hangman4.js
@@ -0,0 +1,25 @@
+//readLineSync
+
+//create word
+
+//split word
+
+//create blank line word to fill in
+
+//create trackers
+
+
+//display results
+
+//game
+    //create prompt
+    
+    // check guess against letters
+
+   //handle incorrect guesses
+
+    //handle winning
+
+    //handle losing
+
+    //call for continuous guess
