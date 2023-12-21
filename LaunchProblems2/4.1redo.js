ef4a2e9321604a958ce7583f7f6822e75eb28f71
Todd Garrison <toddlgarrison@gmail.com>
2023-12-21 16:33:44 -0500

    redo

--- LaunchProblems2/4.1redo.js
+++ LaunchProblems2/4.1redo.js
@@ -4,7 +4,13 @@
 const arrayOfArrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 function flattenArray(array) {
+    let combinedArray = []
 
+    for (let i = 0; i < array.length; i++) {
+        combinedArray = combinedArray.concat(array[i])
+    }
+
+    return combinedArray
 }
 
 console.log('test: ', flattenArray(arrayOfArrays));
