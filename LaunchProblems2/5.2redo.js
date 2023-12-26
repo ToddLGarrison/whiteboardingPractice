7930c357b3bc3919bc49be68b53fb155336dc9ae
Todd Garrison <toddlgarrison@gmail.com>
2023-12-26 12:32:31 +0100

    redo solution

--- LaunchProblems2/5.2redo.js
+++ LaunchProblems2/5.2redo.js
@@ -1,11 +1,18 @@
 //given a word check if it is a palindrome return true or false
 
 function findPalindrome(word) {
+    const length = word.length
 
+    for(let i = 0; i < length / 2; i++) {
+        if(word[i] !== word[length - 1 - i]) {
+            return false
+        }
+    }
+    return true
 }
 
 
-console.log('test 1:', findPalindrome('toot'))
-console.log('test 2:', findPalindrome('fact'))
+console.log('test 1:', findPalindrome('local'))
+console.log('test 2:', findPalindrome('wooooooow'))
 console.log('test 3:', findPalindrome('racecar'))
 console.log('test 4:', findPalindrome('tort'))
