0a853077e7c7d5b55cf5d7f6a10012d9e79d702d
Todd Garrison <toddlgarrison@gmail.com>
2023-12-26 06:08:17 -0500

    testing alt solutions

--- LaunchProblems2/5.1noVowels.js
+++ LaunchProblems2/5.1noVowels.js
@@ -1,9 +1,9 @@
 // Given a word, create a function that returns the same word without any vowels
 
-word = 'vegetable'
+word = 'vegetable are great for your digestion'
 
 let removeVowels = (string) => {
-    return string.replace(/[aeiou]/g, "")
+    return string.replace(/[aeiou]/gi, "")
 }
 
 console.log('test: ', removeVowels(word))
