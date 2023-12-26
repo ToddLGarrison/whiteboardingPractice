9b4a3d4f8a3f714fe2c9e0a14a2da0992c015cd8
Todd Garrison <toddlgarrison@gmail.com>
2023-12-26 05:57:01 -0500

    complete

--- LaunchProblems2/4.1redo2.js
+++ LaunchProblems2/4.1redo2.js
@@ -6,7 +6,11 @@ const arrayOfArrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 const oneArray = (array) => {
     let oneTrueArray = []
 
-    
+    for(let i = 0; i < array.length; i++) {
+        oneTrueArray = oneTrueArray.concat(array[i])
+    }
+
+    return oneTrueArray
 }
 
 console.log('test ', oneArray(arrayOfArrays))
