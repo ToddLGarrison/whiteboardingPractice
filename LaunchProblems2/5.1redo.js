2fc89beadfb4fb8f672ae3aaa39d28b2dd5b3f20
Todd Garrison <toddlgarrison@gmail.com>
2023-12-26 06:09:56 -0500

    redo

---
+++ LaunchProblems2/5.1redo.js
@@ -0,0 +1,9 @@
+// Given a word, create a function that returns the same word without any vowels
+
+string = 'vegetable are great for your digestion'
+
+function removeVowels(word) {
+
+}
+
+console.log('test: ', removeVowels(string));
