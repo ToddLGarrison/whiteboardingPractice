af51866d12ce1528d7409d1012538a30182bc753
Todd Garrison <toddlgarrison@gmail.com>
2023-12-26 05:54:59 -0500

    redo arrrayOfArrays

---
+++ LaunchProblems2/4.1redo2.js
@@ -0,0 +1,12 @@
+// Given an array of arrays, create a function that puts all elements in a single array 
+// (without using the JS Array method .flat())
+
+const arrayOfArrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+const oneArray = (array) => {
+    let oneTrueArray = []
+
+    
+}
+
+console.log('test ', oneArray(arrayOfArrays))
