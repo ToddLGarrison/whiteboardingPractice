11c9a9257b79d9a94ce26d3bdcc37019dd22b37a
Todd Garrison <toddlgarrison@gmail.com>
2023-12-26 12:26:44 +0100

    solution

--- LaunchProblems2/5.1findPalindrome.js
+++ LaunchProblems2/5.1findPalindrome.js
@@ -1,11 +1,18 @@
 //given a word check if it is a palindrome return true or false
 
-function findPalindrome() {
-    
+function findPalindrome(word) {
+    const length = word.length
+
+    for(let i = 0; i < length / 2; i++) {
+        if(word[i] !== word[length - 1 - i]) {
+            return false
+        }
+    }
+    return true
 }
 
 
-console.log('test 1:', findPalindrome('toot') === true)
-console.log('test 2:', findPalindrome('fact') === false)
-console.log('test 3:', findPalindrome('racecar') === true)
-console.log('test 4:', findPalindrome('tort') === false)
+console.log('test 1:', findPalindrome('toot'))
+console.log('test 2:', findPalindrome('fact'))
+console.log('test 3:', findPalindrome('racecar'))
+console.log('test 4:', findPalindrome('tort'))
