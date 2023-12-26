abf578c1e481b03051c238fb081c5a38aa20da5d
Todd Garrison <toddlgarrison@gmail.com>
2023-12-26 06:01:50 -0500

    noVowels problem

---
+++ LaunchProblems2/5.1noVowels.js
@@ -0,0 +1,18 @@
+// Given a word, create a function that returns the same word without any vowels
+
+word = 'vegetable'
+
+let removeVowels = (string) => {
+    let noVowels = ''
+    let splitWord = string.split('')
+
+    for(let i = 0; i < splitWord.length; i++) {
+        
+    }
+
+    noVowels.join('')
+
+    return noVowels
+}
+
+console.log('test: ', removeVowels(word))
