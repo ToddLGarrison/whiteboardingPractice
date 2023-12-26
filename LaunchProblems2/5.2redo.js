6a6206fe5fdb7103a4404412052e3f9c21cf9352
Todd Garrison <toddlgarrison@gmail.com>
2023-12-26 12:27:33 +0100

    redo findPalindrome

---
+++ LaunchProblems2/5.2redo.js
@@ -0,0 +1,11 @@
+//given a word check if it is a palindrome return true or false
+
+function findPalindrome(word) {
+
+}
+
+
+console.log('test 1:', findPalindrome('toot'))
+console.log('test 2:', findPalindrome('fact'))
+console.log('test 3:', findPalindrome('racecar'))
+console.log('test 4:', findPalindrome('tort'))
