85881c2ba11c62bcd11f6baa77985d748cdd4ee1
Todd Garrison <toddlgarrison@gmail.com>
2023-12-31 09:12:55 +0100

    factorial function

---
+++ LaunchProblems2/7.1factorial.js
@@ -0,0 +1,15 @@
+//write a function that creates a factorial for an input
+
+function createFactorial(x) {
+    let total = 1
+
+    for(let i = 0; i <= x.length; i++) {
+    
+            total * 1
+        
+    }
+
+    return total
+}
+
+console.log('test: ', createFactorial(3))
