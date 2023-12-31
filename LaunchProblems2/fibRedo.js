80e4a929aeb6d9cb66c3a594bb0fcc8c7e245078
Todd Garrison <toddlgarrison@gmail.com>
2023-12-31 09:43:00 +0100

    redo fib seq

--- LaunchProblems2/fibRedo.js
+++ LaunchProblems2/fibRedo.js
@@ -3,9 +3,12 @@
 function fibonacciSequence(x) {
     let sequence = [0, 1]
 
-    for (let i = 2; i <= x; i++)
+    for (let i = 2; i <= x; i++){
+        let nextNumber = sequence[sequence.length - 2] + sequence[sequence.length - 1]
+        sequence.push(nextNumber)
+    }
     return sequence
 }
 
 
-console.log('Test: ', fibonacciFunction(10))
+console.log('Test: ', fibonacciSequence(10))
