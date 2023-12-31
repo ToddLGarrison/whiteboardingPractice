f0ffe87350dc2ee12b451082433356e0c8adf303
Todd Garrison <toddlgarrison@gmail.com>
2023-12-31 09:40:20 +0100

    redo fib

---
+++ LaunchProblems2/fibRedo.js
@@ -0,0 +1,11 @@
+//write a function that will output the fibonacci sequence to an input 
+
+function fibonacciSequence(x) {
+    let sequence = [0, 1]
+
+    for (let i = 2; i <= x; i++)
+    return sequence
+}
+
+
+console.log('Test: ', fibonacciFunction(10))
