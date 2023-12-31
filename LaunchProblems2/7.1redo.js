9986d3116aae208d79617ab88dd8831657d6e35b
Todd Garrison <toddlgarrison@gmail.com>
2023-12-31 09:23:54 +0100

    factorial redo

---
+++ LaunchProblems2/7.1redo.js
@@ -0,0 +1,13 @@
+//write a function that creates a factorial for an input
+
+function factorial(x){
+    let product = 1
+
+    for (let i = 1; i < x; i++) {
+        product *= i
+    }
+
+    return product
+}
+
+console.log('test: ',  factorial(6))
