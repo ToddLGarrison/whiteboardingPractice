b4adb44081314e316206f2ee8013913171103924
Todd Garrison <toddlgarrison@gmail.com>
2023-12-31 09:37:24 +0100

    solution

--- LaunchProblems2/fibonacci.js
+++ LaunchProblems2/fibonacci.js
@@ -4,11 +4,11 @@ function fibonacciFunction(x){
     let sequence = [0, 1]
 
     for (let i = 2; i <= x; i++) {
-        let nextNumber = sequence[sequence - 2] + sequence[sequence - 1]
-        
+        let nextNumber = sequence[sequence.length - 2] + sequence[sequence.length - 1]
+        sequence.push(nextNumber)
     }
 
     return sequence
 }
 
-console.log('Test: ', fibonacciFunction(5))
+console.log('Test: ', fibonacciFunction(10))
