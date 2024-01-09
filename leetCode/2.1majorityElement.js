33728ba12e8f0a3689e5a5357506a3b1b98e30a2
Todd Garrison <toddlgarrison@gmail.com>
2024-01-09 15:55:46 -0500

    solution

--- leetCode/2.1majorityElement.js
+++ leetCode/2.1majorityElement.js
@@ -1,3 +1,26 @@
-function majorityElement (num) {
+function majorityElement (nums) {
+    let countMap = {}
     
+    for (const num of nums){
+        if(countMap[num] !== undefined) {
+            countMap[num]++
+        } else {
+            countMap[num] = 1
+        }
+    }
+
+    let majorityElement = nums[0]
+    let maxCount = countMap[nums[0]]
+
+    for (const num in countMap) {
+        if(countMap[num] > maxCount) {
+            majorityElement = num
+            maxCount = countMap[num]
+        }
+    }
+
+    return countMap
 }
+
+let array = [2,2,1,1,1,2,2]
+console.log('test: ', majorityElement(array))
