3469fee42286061b7a2d06741fde32df54b6cbd6
Todd Garrison <toddlgarrison@gmail.com>
2024-01-14 15:51:21 -0500

    fix error

--- leetCode/4.1palidrome.js
+++ leetCode/4.1palidrome.js
@@ -3,10 +3,12 @@
 const isPalindrome = (x) => {
     let string = x.toString();
 
-    const reverseString = string.reverse();
+    const reverseString = string.split('').reverse().join('');
 
     return string === reverseString
 }
 
 console.log('test 1:', isPalindrome(12321))
 console.log('test 2:', isPalindrome(12321512512))
+console.log('test 3:', isPalindrome('racar'))
+console.log('test 4:', isPalindrome('dog'))
