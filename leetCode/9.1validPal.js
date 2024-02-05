f4de4631b664d8f984f94759421fc156825eec7b
Todd Garrison <toddlgarrison@gmail.com>
2024-02-05 15:42:32 -0500

    refactor

--- leetCode/9.1validPal.js
+++ leetCode/9.1validPal.js
@@ -2,19 +2,8 @@ var isPalindrome = function(s) {
     if (s.length <= 1) {
         return true;
     }
-    //make lowercase - done
-    //split s - done
-    //remove all non characters - done
-    //reverse
-    //join and compare
 
-    let charactersOnly = s.toLowerCase().replace(/[^a-z]/g, '');
-
-    if (charactersOnly === charactersOnly.split('').reverse().join('')){
-        return true;
-    }
-
-    return charactersOnly
+    return s.toLowerCase().replace(/[^a-z]/g, '') === s.toLowerCase().replace(/[^a-z]/g, '').split('').reverse().join('')
 };
 
 console.log(isPalindrome("A man, a plan, a canal: Panama"))
