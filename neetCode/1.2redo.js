8e117504b3fbf2e033e8a8856355b05431510dc9
Todd Garrison <toddlgarrison@gmail.com>
2024-02-12 14:30:54 -0500

    twoSums problem and solution

---
+++ neetCode/1.2redo.js
@@ -0,0 +1,25 @@
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    let sHash = {}
+    let tHash = {}
+
+    for (let char of s) {
+        sHash[char] = (sHash[char] || 0) + 1
+    }
+
+    for (let char of t) {
+        tHash[char] = (tHash[char] || 0) +1
+    }
+
+    for (let char in sHash) {
+        if (sHash[char] !== tHash[char]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+console.log(isAnagram('rat', 'car'))
+console.log(isAnagram('anagram', 'nagaram'))
---
+++ neetCode/1.3twoSums.js
@@ -0,0 +1,17 @@
+var twoSum = function(nums, target) {
+    let output = []
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i]
+
+        if (output.hasOwnProperty(complement)) {
+            return [output[complement], i]
+        }
+        output[nums[i]] = i
+    }
+
+    return output
+};
+
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3, 4, 2, 7, 11, 15], 9))
