52fd75753f7cedba776c73fc80aaa7e86d353078
Todd Garrison <toddlgarrison@gmail.com>
2024-03-19 15:16:46 -0400

    oddInt problem

---
+++ LaunchProblems3/1.1oddIntegers.js
@@ -0,0 +1,9 @@
+// Given an array of integers, return a list of all odd numbers found in the list
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+const oddNumbers = (array) => {
+    
+}
+
+console.log("Test 1", oddNumbers(numbers))
