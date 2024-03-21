2311db3f1db253480350b105c47c8707ae344d27
Todd Garrison <toddlgarrison@gmail.com>
2024-03-21 16:23:26 -0400

    attempt 3

--- AlgoExpert/twoNumSum.js
+++ AlgoExpert/twoNumSum.js
@@ -14,23 +14,20 @@
 
 
 function twoNumSum(array, targetSum) {
-    array.sort((a,b) => a - b);
-    let left = 0;
-    let right = array.length - 1;
+    array.sort((a, b) => a - b)
+    let left = 0
+    let right = array.length - 1
 
     while (left < right) {
-        console.log(array[left], array[right])
-        const currentSum = array[left] + array [right]; 
-
+        let currentSum = array[left] + array[right]
         if (currentSum === targetSum) {
-            return [array[left], array[right]];
+            return [array[left], array[right]]
         } else if (currentSum < targetSum) {
             left++
         } else if (currentSum > targetSum) {
             right--
         }
     }
-
     return []
 }
 
