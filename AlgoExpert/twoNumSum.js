c64577ff251fb2d441a643295da7c85360be094c
Todd Garrison <toddlgarrison@gmail.com>
2024-03-21 15:44:05 -0400

    Faster solution

--- AlgoExpert/twoNumSum.js
+++ AlgoExpert/twoNumSum.js
@@ -1,12 +1,32 @@
-function twoNumSum(array, targetSum) {
-    let nums = {}
+// function twoNumSum(array, targetSum) {
+//     let nums = {}
 
-    for (const num of array) {
-        const potentialMatch = targetSum - num
-        if (potentialMatch in nums){
-            return [potentialMatch, num];
-        } else {
-            nums[num] = true;
+//     for (const num of array) {
+//         const potentialMatch = targetSum - num
+//         if (potentialMatch in nums){
+//             return [potentialMatch, num];
+//         } else {
+//             nums[num] = true;
+//         }
+//     }
+
+//     return []
+
+
+function twoNumSum(array, targetSum) {
+    array.sort((a,b) => {a - b});
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left < right) {
+        const currentSum = array[left] + array [right]; 
+
+        if (currentSum === targetSum) {
+            return [array[left], array[right]];
+        } else if (currentSum < targetSum) {
+            left++
+        } else if (currentSum > targetSum) {
+            right--
         }
     }
 
@@ -17,3 +37,4 @@ let array = [3, 5, -4, 8, 11, 1, -1, 6]
 let targetSum = 10
 
 console.log(twoNumSum(array, targetSum))
+
