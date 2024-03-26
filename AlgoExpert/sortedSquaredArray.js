529f8b774d3e1cf18f41bb8c03c6097372b0d0b2
Todd Garrison <toddlgarrison@gmail.com>
2024-03-26 14:57:20 -0400

    Second solution

--- AlgoExpert/sortedSquaredArray.js
+++ AlgoExpert/sortedSquaredArray.js
@@ -1,15 +1,29 @@
+// function sortedSquaredArray(array) {
+//     let squaredArray = new Array(array.length).fill(0);
+
+//     for (let i = 0; i < array.length; i++) {
+//         let value = array[i]
+//         squaredArray[i] = value * value 
+//     }
+    
+//     squaredArray.sort((a, b) => a - b)
+
+//     return squaredArray
+// }
+
+
 function sortedSquaredArray(array) {
     let squaredArray = new Array(array.length).fill(0);
+    let smallerValueIndex = 0;
+    let largerValueIndex = array.length - 1;
 
     for (let i = 0; i < array.length; i++) {
-        let value = array[i]
-        squaredArray[i] = value * value 
+        
     }
-    
-    squaredArray.sort((a, b) => a - b)
 
-    return squaredArray
+    return squaredArray;
 }
+
 
 // let array = [1, 2, 3, 5, 6, 8, 9]
 let array = [1, 4, 9, 16, 25]
