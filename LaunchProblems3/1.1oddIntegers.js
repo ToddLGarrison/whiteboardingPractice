44d6917b40d49cc2ae09988ab63d14f670dd6a3d
Todd Garrison <toddlgarrison@gmail.com>
2024-04-07 16:20:27 -0400

    odd array problem

--- LaunchProblems3/1.1oddIntegers.js
+++ LaunchProblems3/1.1oddIntegers.js
@@ -3,15 +3,16 @@
 let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
 const oddNumbers = (array) => {
-    let oddNumbers = []
+    const oddArray = []
 
-    for (let i = 0; i < array.length; i++) {
-        if(array[i] % 2 !== 0) {
-            oddNumbers.push(array[i])
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] % 2 !==0) {
+            oddArray.push(array[i])
         }
     }
 
-    return oddNumbers
+    return oddArray
+
 }
 
 console.log("Test 1", oddNumbers(numbers))
