af6c6562a9b690d499307c6190304b4ff03ca9d3
Todd Garrison <toddlgarrison@gmail.com>
2024-04-08 15:41:01 -0400

    recursive solution

--- AlgoExpert/bubbleSort.js
+++ AlgoExpert/bubbleSort.js
@@ -1,34 +1,40 @@
-function bubbleSort(array) {
-    let isSorted = false;
-    let counter = 0;
-    while(!isSorted) {
-        for (let i = 0; i < array.length - 1 - counter; i++) {
-            isSorted = true;
-            if (array[i] > array[i + 1]) {
-                swap(i, i + 1, array)
-                isSorted = false
-            }
-        }
-        counter++
-    }
-
-    return array;
-}
-
-function swap(i, j, array) {
-    const temp = array[j]
-    array[j] = array[i]
-    array[i] = temp
-}
-
-
-//recursive solution
-
 // function bubbleSort(array) {
-//     let swaps = 0
+//     let isSorted = false;
+//     let counter = 0;
+//     while(!isSorted) {
+//         for (let i = 0; i < array.length - 1 - counter; i++) {
+//             isSorted = true;
+//             if (array[i] > array[i + 1]) {
+//                 swap(i, i + 1, array)
+//                 isSorted = false
+//             }
+//         }
+//         counter++
+//     }
 
-//     for (let i = 0; i < array.length - 1; i++) {}
+//     return array;
 // }
+
+// function swap(i, j, array) {
+//     const temp = array[j]
+//     array[j] = array[i]
+//     array[i] = temp
+// }
+
+
+// recursive solution
+
+function bubbleSort(array) {
+    let swaps = 0
+
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            [array[i], array[i + 1]] = [array[i + 1], array[i]]
+            swaps++
+        }
+    }
+    return swaps === 0 ? array : bubbleSort(array)
+}
 
 let array = [8, 5, 2, 9, 5, 6, 3]
 
