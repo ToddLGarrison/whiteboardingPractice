941e490c60b2159707a21fb7630d38ec4360f617
Todd Garrison <toddlgarrison@gmail.com>
2024-04-08 14:34:27 -0400

    bubble sort problem and solution

---
+++ AlgoExpert/bubbleSort.js
@@ -0,0 +1,26 @@
+function bubbleSort(array) {
+    let isSorted = false;
+    let counter = 0
+    while (!isSorted) {
+        isSorted = true
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(i, i + 1, array)
+                isSorted = false
+            }
+        }
+        counter++
+    }
+    return array
+}
+
+function swap(i, j, array) {
+    const temp = array[j]
+    array[j] = array[i]
+    array[i] = temp
+}
+
+
+let array = [8, 5, 2, 9, 5, 6, 3]
+
+console.log(bubbleSort(array))
