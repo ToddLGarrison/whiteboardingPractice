163b283c461e9d18bb92455b51e4db0cbcb8e480
Todd Garrison <toddlgarrison@gmail.com>
2024-04-09 16:34:47 -0400

    Memoize Solution

--- AlgoExpert/getFibNum.js
+++ AlgoExpert/getFibNum.js
@@ -14,15 +14,30 @@
 
 // simple attempt but slower
 
+// function getNthFib(n) {
+//     if (n === 2) {
+//         return 1
+//     } else if (n === 1) {
+//         return 0
+//     } else {
+//         return getNthFib(n - 1) + getNthFib(n - 2)
+//     }
+// }
+
+//memoize solution
+
 function getNthFib(n) {
-    if (n === 2) {
-        return 1
-    } else if (n === 1) {
-        return 0
+    let  memoize = {
+        1: 0,
+        2: 1
+    }
+
+    if (n in memoize) {
+        return memoize[n]
     } else {
-        return getNthFib(n - 1) + getNthFib(n - 2)
+        memoize[n] = getNthFib(n -1, memoize) + getNthFib(n - 2, memoize)
+        return memoize[n]
     }
 }
-
 
 console.log(getNthFib(6))
