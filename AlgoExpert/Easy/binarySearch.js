73ab9d776a115d87aa9b435e2b95bb38717779ec
Todd Garrison <toddlgarrison@gmail.com>
2024-04-10 10:08:47 -0400

    Solution 3

--- AlgoExpert/Easy/binarySearch.js
+++ AlgoExpert/Easy/binarySearch.js
@@ -13,21 +13,38 @@ function binarySearch(array, target) {
 }
 
 //helper function 1
-function binarySearchHelper(array, target, left, right) {
-    if (left > right) return -1
+// function binarySearchHelper(array, target, left, right) {
+//     if (left > right) return -1
 
-    const middle = Math.floor((left + right) / 2)
-    const potentialMatch = array[middle]
+//     const middle = Math.floor((left + right) / 2)
+//     const potentialMatch = array[middle]
 
-    if(target === potentialMatch) {
-        return middle
-    } else if (target < potentialMatch) {
-        return binarySearchHelper(array, target, left, middle - 1)
-    } else {
-        return binarySearchHelper(array, target, middle + 1, right)
+//     if(target === potentialMatch) {
+//         return middle
+//     } else if (target < potentialMatch) {
+//         return binarySearchHelper(array, target, left, middle - 1)
+//     } else {
+//         return binarySearchHelper(array, target, middle + 1, right)
+//     }
+// }
+
+// helper function 2
+
+function binarySearchHelper(array, target, left, right){
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2)
+        const potentialMatch = array[middle]
+
+        if(target === potentialMatch) {
+            return middle
+        } else if (target < potentialMatch) {
+            right = middle - 1
+        } else {
+            left = middle + 1
+        }
     }
+    return -1
 }
-
 
 const array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
 
