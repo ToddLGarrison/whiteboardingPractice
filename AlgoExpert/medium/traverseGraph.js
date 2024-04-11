a6a5721521b13171811eb571e20109bf954f9fdb
Todd Garrison <toddlgarrison@gmail.com>
2024-04-11 10:45:03 -0400

    TraverseGraph problem and recursive solution

---
+++ AlgoExpert/medium/traverseGraph.js
@@ -0,0 +1,9 @@
+function numberOfWaysToTraverseGraph(width, height) {
+    if (width === 1 || height === 1) return 1
+
+    return (
+        numberOfWaysToTraverseGraph(width - 1, height) + numberOfWaysToTraverseGraph(width, height - 1)
+    );
+}
+
+console.log(numberOfWaysToTraverseGraph(4, 3))
