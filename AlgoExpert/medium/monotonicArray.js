53c2e4b19e53a98fcc85d310fc925df5369e6425
Todd Garrison <toddlgarrison@gmail.com>
2024-04-18 15:57:02 -0400

    Monotonic problem and solution

---
+++ AlgoExpert/medium/monotonicArray.js
@@ -0,0 +1,12 @@
+function isMonotonic(array) {
+    let isNonDecreasing = true;
+    let isNonIncreasing = true
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < array[i - 1]) isNonDecreasing = false;
+        if (array[i] > array[i - 1]) isNonIncreasing = false;
+    }
+    return isNonDecreasing || isNonIncreasing
+}
+
+console.log(isMonotonic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001])) 
